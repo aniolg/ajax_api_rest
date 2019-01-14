f820a7befd71fbd97ff0c87c2c56426fe312a4ab
aniolg <aniol_95@hotmail.com>
2019-01-14 11:10:00 +0100

    ajax handlers added

--- request.js
+++ request.js
@@ -21,8 +21,27 @@ function addData(result) {
     
 }
 
-function errorMessage(xhr, status, strErr){
-    console.log("There was an error!");
-}
+/*AJAX HANDLERS*/
+$(document).ajaxStart(function () {
+    console.log("AJAX starting");
+});
 
+$(document).ajaxStop(function () {
+    console.log("AJAX request ended");
+});
 
+$(document).ajaxSend(function (evt, jqXHR, options) {
+    console.log("About to Send request for data...");
+});
+
+$(document).ajaxComplete(function (evt, jqXHR, options) {
+    console.log("Everything's finished!");
+});
+
+$(document).ajaxError(function (evt, jqXHR, settings, err) {
+    console.log("Hmmm. Seems like there was a problem: " + err);
+});
+
+$(document).ajaxSuccess(function (evt, jqXHR, options) {
+    console.log("Looks like everything worked!");
+});
